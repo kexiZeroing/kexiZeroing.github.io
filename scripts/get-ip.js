a8b0b8e26a2dd3f206c629d0bea389bd4879cbd0
Kexi Dang <dangkexi@gmail.com>
2023-06-17 10:30:07 +0800

    get ip address script

---
+++ scripts/get-ip.js
@@ -0,0 +1,34 @@
+import os from "os";
+import fetch from 'node-fetch';
+
+function getPrivateIp() {
+  const interfaces = os.networkInterfaces();
+
+  for (let devName in interfaces) {
+    let iface = interfaces[devName];
+    for (let i = 0; i < iface.length; i++) {
+      let alias = iface[i];
+      if (
+        alias.family === "IPv4" &&
+        alias.address !== "127.0.0.1" &&
+        !alias.internal
+      ) {
+        return alias.address;
+      }
+    }
+  }
+  return "";
+}
+
+async function getPublicIp(useIPv6) {
+  // https://www.ipify.org
+  const endpoint = useIPv6 ? "https://api6.ipify.org" : "https://api.ipify.org";
+
+  const response = await fetch(endpoint);
+  return await response.text();
+}
+
+(async () => {
+  console.log('private ip:', getPrivateIp());
+  console.log('public ip:', await getPublicIp());
+})()
