0c6883043604f41cc02245d0f61749c06ef76c72
Kexi Dang <dangkexi@gmail.com>
2023-11-19 15:07:34 +0800

    simple screen recorder script

---
+++ scripts/simple-screen-recorder.js
@@ -0,0 +1,23 @@
+// https://dev.to/ninofiliu/simple-screen-recorder-in-20-lines-of-javascript-4ina
+
+const button = document.createElement("button");
+button.innerHTML = "capture";
+document.body.append(button);
+
+button.addEventListener("click", async () => {
+  const stream = await navigator.mediaDevices.getDisplayMedia();
+  const recoder = new MediaRecorder(stream);
+  recoder.start();
+
+  const [video] = stream.getVideoTracks();
+  video.addEventListener("ended", () => {
+    recoder.stop();
+  });
+
+  recoder.addEventListener("dataavailable", (evt) => {
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(evt.data);
+    a.download = "capture.webm";
+    a.click();
+  });
+});
