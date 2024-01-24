70ce282485542aba652dc32da8247153e1f2830a
Kexi Dang <dangkexi@gmail.com>
2024-01-24 12:09:44 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Blog | Kexi Dang';
+export const SITE_TITLE = 'Kexi Dang';
 export const SITE_DESCRIPTION = 'Kexi\'s blog, Software Engineer, 微信公众号：一切都在成长中';
 export const SITE_TITLE_MOBILE = 'Kexi\'s blog';
