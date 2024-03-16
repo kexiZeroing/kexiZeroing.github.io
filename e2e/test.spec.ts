08a2bb3bc00244be7358dfa76ea31f31d5c16622
Kexi Dang <dangkexi@gmail.com>
2024-03-16 16:59:03 +0800

    add playwright e2e tests

---
+++ e2e/test.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+// VSCode Testing -> Record new
+test.describe('My Blog', () => {
+  test('should load page', async ({ page }) => {
+    await page.goto('https://kexizeroing.github.io/');
+    
+    await expect(page.getByRole('link', { name: 'Home' })).toHaveClass(/active/);
+  });
+
+  test('has blog title', async ({ page }) => {
+    await page.goto('https://kexizeroing.github.io/');
+    await page.getByRole('link', { name: 'Posts' }).click();
+    await page.getByRole('link', { name: 'TIL' }).click();
+    
+    await expect(page).toHaveTitle(/Blog - TIL/);
+  });
+
+  test('should toggle theme', async ({ page }) => {
+    await page.goto('https://kexizeroing.github.io/');
+    await page.getByLabel('switch theme').click();
+    
+    await expect(page.locator('html')).toHaveClass(/theme-dark/);
+  });
+});
