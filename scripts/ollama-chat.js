638fd29dedae852dc4c6f8b9279513057e4c1b06
Kexi Dang <dangkexi@gmail.com>
2024-06-09 12:34:42 +0800

    update

--- scripts/ollama-chat.js
+++ scripts/ollama-chat.js
@@ -1,6 +1,4 @@
 // https://github.com/ollama/ollama-js
-// ollama run gemma:2b
-
 import ollama from 'ollama'
 
 // const response = await ollama.chat({
@@ -18,3 +16,22 @@ const response = await ollama.chat({
 for await (const part of response) {
   process.stdout.write(part.message.content)
 }
+
+// https://js.langchain.com/v0.2/docs/integrations/llms/ollama/
+// import { Ollama } from "@langchain/community/llms/ollama";
+
+// const ollama = new Ollama({
+//   baseUrl: "http://localhost:11434", // Default value
+//   model: "llama2", // Default value
+// });
+
+// const stream = await ollama.stream(
+//   `Translate "I love programming" into German.`
+// );
+
+// const chunks = [];
+// for await (const chunk of stream) {
+//   chunks.push(chunk);
+// }
+
+// console.log(chunks.join(""));
