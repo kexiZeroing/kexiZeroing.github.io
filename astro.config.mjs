914744ed0d033a80b82b2d1fca01d3f85c9f65d5
Kexi Dang <dangkexi@gmail.com>
2024-07-14 11:41:03 +0800

    integrate with shadcn ui and add a page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 // https://docs.astro.build/en/guides/integrations-guide/mdx/
 export default defineConfig({
   site: 'https://kexizeroing.github.io',
-  integrations: [mdx(), react()]
+  integrations: [
+    mdx(),
+    react(), 
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ]
 });
