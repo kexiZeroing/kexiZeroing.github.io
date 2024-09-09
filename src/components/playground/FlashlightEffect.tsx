630d639161e19fa1424ed9c9d9c7bb0ab3c27c2a
Kexi Dang <dangkexi@gmail.com>
2024-09-09 20:08:10 +0800

    flashlight effect demo

---
+++ src/components/playground/FlashlightEffect.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState } from 'react';
+
+const FlashlightEffect = () => {
+  const [gradient, setGradient] = useState(null);
+
+  const handleMouseMove = (e) => {
+    const { pageX: x, pageY: y } = e;
+    // transition from transparent to black within the first 15% of the circle’s radius.
+    setGradient(`radial-gradient(circle at ${x}px ${y}px, transparent, #000 15%)`);
+  };
+
+  return (
+    <section
+      onMouseMove={handleMouseMove}
+      className="relative w-full h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url('https://kexizeroing.github.io/og-image.jpg')`,
+      }}
+    >
+      <div
+        className="absolute inset-0 bg-black"
+        style={gradient ? { background: gradient } : {}}
+      ></div>
+    </section>
+  );
+};
+
+export default FlashlightEffect;
+
